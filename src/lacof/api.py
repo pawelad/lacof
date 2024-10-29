3497f953375890437b4534750835214bf1df5d71
Paweł Adamczak <pawel.ad@gmail.com>
2024-10-29 23:12:44 +0100

    Add `info` API view

--- src/lacof/api.py
+++ src/lacof/api.py
@@ -1,8 +1,26 @@
 """Main lacof API routes config."""
 
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
 
 from images.routes import images_router
+from lacof import __version__
+from lacof.settings import lacof_settings
+from lacof.utils import APIInfo
+from users.auth import get_current_user
+from users.schemas import User
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(images_router)
+
+
+@api_router.get("/info")
+async def info(user: Annotated[User, Depends(get_current_user)]) -> APIInfo:
+    """Show API info."""
+    api_info = APIInfo(
+        version=__version__,
+        environment=lacof_settings.ENVIRONMENT,
+        user=user.name,
+    )
+    return api_info
--- src/lacof/utils.py
+++ src/lacof/utils.py
@@ -1,6 +1,7 @@
 """Lacof app utils."""
 
 from fastapi.openapi.constants import REF_PREFIX
+from pydantic import BaseModel
 
 # Source: https://github.com/fastapi/fastapi/blob/adf89d1d9fdc8ea03bc0f3361b3d5e4b6835cf6c/fastapi/openapi/utils.py#L404-L411
 API_ERROR_SCHEMA = {
@@ -10,3 +11,17 @@ API_ERROR_SCHEMA = {
         }
     }
 }
+
+
+class APIInfo(BaseModel):
+    """API info schema.
+
+    Attributes:
+        version: App version. Not to be confused with API version.
+        environment: App environment. Can be one either "local" or "production".
+        user: Current API user.
+    """
+
+    version: str
+    environment: str
+    user: str
