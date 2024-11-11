3cddea1f75731a1709c2c51559e850c9c24299e0
Paweł Adamczak <pawel.ad@gmail.com>
2024-11-11 01:45:00 +0100

    Dev version bump

--- src/lacof/__init__.py
+++ src/lacof/__init__.py
@@ -1,7 +1,7 @@
 """Lacof."""
 
 __title__ = "lacof"
-__version__ = "0.1.0"
+__version__ = "0.1.1.dev0"
 __url__ = "https://github.com/pawelad/lacof"
 __author__ = "Paweł Adamczak"
 __email__ = "pawel.ad@gmail.com"
